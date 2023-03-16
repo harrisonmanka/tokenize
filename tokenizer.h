aa06967d324cb6f9e87aef84ae22cab195fbd7d2
Matthew_Agudelo <magudeloganan1@catamount.wcu.edu>
2023-03-16 16:09:02 -0400

    define for operators

--- tokenizer.h
+++ tokenizer.h
@@ -8,7 +8,16 @@
 #define TSIZE 20
 #define TRUE 1
 #define FALSE 0
-
+#define MULT_OP "*"
+#define ADD_OP "+"
+#define DIV_OP "-"
+#define ASSIGN_OP "="
+#define LESS_THAN "<"
+#define EQUAL_OP "=="
+#define NOT_OP "!"
+#define GREATER_THAN ">"
+#define NOT_EQUAL_SIGN "!="
+#define SEMI_COLON ";"
 /**
 * add comment
 */
