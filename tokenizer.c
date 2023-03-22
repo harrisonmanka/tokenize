4aeecf473b127cca5bf320f9b2842cc427ce817a
Matthew_Agudelo <magudeloganan1@catamount.wcu.edu>
2023-03-22 14:02:16 -0400

    getting some of the tokens recognized

--- tokenizer.c
+++ tokenizer.c
@@ -21,6 +21,7 @@
 char *line;             // Global pointer to line of input
 // (optional) can declare some additional variable if you want to
 
+char* token_type = "";
 /**
 * add comment
 */
@@ -29,6 +30,7 @@ int main(int argc, char* argv[]) {
    char  input_line[LINE];  /* Line of input, fixed size        */
    FILE  *in_file = NULL;        /* File pointer                     */
    FILE  *out_file = NULL;
+   
    int   line_count,        /* Number of lines read             */
          start,    /* is this the start of a new statement? */
          count;             /* count of tokens                  */
@@ -51,38 +53,48 @@ int main(int argc, char* argv[]) {
   }
 
    // (optional) can add some code here if you want some here
- 
+   int cnt = 0;
    while (fgets(input_line, LINE, in_file) != NULL)
    {
       line = input_line;  // Sets a global pointer to the memory location
                            // where the input line resides.
-      //remove_whitespace(line);
-      //printf("%s", line);
-      //identify_lexeme(line);
       // Add code here. Keep this file no longer than 50 lines of code.
       // Use helper functions.
-      while(*line != ";"){
-          get_token(line);
+      printf("%s", "-----------------------------------------------\n");
+      count = 0;
+      for(int i = 0; i < strlen(line);){
+        if(line[i] == ' '){
+          i++;
+          cnt++;
       }
-      line_count++;
-
-
+      else{
+        token[0] = line[cnt];
+        cnt++;
+        while(isdigit(line[cnt])){
+            token[cnt] = line[cnt];
+            cnt++;
+        }//end of while
+        get_token(token);
+        //print token line
+        printf("Lexeme " "%d" " is " "%s" " and is an " "%s\n", count, token, token_type);
+        //clear token
+        memset(token, 0, strlen(token));
+        count++;
+        i++;
+      }//end of else
+    }//end of for
+      
+      //just set the token_type
+      
+      
+      
    }
+   
 
    fclose(in_file);
    fclose(out_file);
    return 0;
 }
-
-/// @brief remove whitespace from the current line
-/// @param line - the current line that is being read
-//char* remove_whitespace(char* line){
-  //  int count = 0;
-    //for (int i = 0; line[i]; i++)
-      //  if (line[i] != ' ')
-        //    line[count++] = line[i];
-    //line[count] = '\0';
-//}
 
 
 
@@ -90,72 +102,50 @@ int main(int argc, char* argv[]) {
 * grab token
 */
 void get_token(char *token_ptr){
-  line = token_ptr;
-  char token_arr[] = "";
-  int len = strlen(token_ptr);
-  //iterates through the entire char array
-  for(int i= 0; i < len; i++){
-    //this is checking if the char is an int
-    if(isdigit(token_ptr[i])){
-      token_arr[i] =  token_ptr[i];
-      get_token_type(token_arr[i]);
-      //need to check ahead if next char is also an int
+  //attempt to convert
+  long int value = strtol(token_ptr, NULL, 10);
+    if(value != 0){
+      token_type = "INT_LITERAL";
+    }
+    else if(token_ptr[0] == ADD_OP){
+      token_type = "ADD_OP";
+    }
+    else if(token_ptr[0] == SUB_OP){
+      token_type = "SUB_OP";
+    }
+    else if(token_ptr[0] == MULT_OP){
+      token_type = "MULT_OP";
+    }
+    else if(token_ptr[0] == LESS_THEN_OP){
+      token_type = "LESS_THAN_OP";
+    }
+    else if(token_ptr[0] == DIV_OP){
+      token_type = "DIV_OP";
+    }
+    else if(token_ptr[0] == ' '){
+    }
+    else if(token_ptr[0] == NOT_OP){
+      token_type = "NOT_OP";
+    }
+    else if(token_ptr[0] == ASSIGN_OP){
+      token_type = "ASSIGN_OP";
+    }
+    else if(token_ptr[0] == SEMI_COLON){
+      token_type = "SEMI_COLON";
+    }
+    else if(token_ptr[0] == LEFT_PAREN){
+      token_type = "LEFT_PAREN";
+    }
+    else if(token_ptr[0] == RIGHT_PAREN){
+      token_type = "RIGHT_PAREN";
+    }
 
-    }
-    //since this is a op no need to check ahead same with others
-    if(token_ptr[0] = '+'){
-      token_arr[0] = token_ptr[i];
-      get_token_type(token_arr[0]);
-      
-    }
-  }
+    
 }
-    //reinitialize TOKEN ARRAY
-    //check current line pointer
-    /**if number{
-        add to TOKEN ARRAY
-        check for next number(s)
-        while(line pointer == number)
-            add to TOKEN ARRAY
-    }
-    if letter{
-        add to TOKEN ARRAY
-        check for next letter(s)
-        while(line pointer == number)
-            add to TOKEN ARRAY
-    }
-    if (< > ! =){
-        add to TOKEN ARRAY
-        check for =
-    }
-    if (*){
-        add to TOKEN ARRAY
-        check for **
-    }
-}*/
+
 
 /**
  * get token type
  */
-void get_token_type(char token){
-    switch(token){
-        case '+':
-        //print to file maybe
-        //fprintf(out_file, "Token" + ADD_OP + " is a " );
-            break;
-        case '-':
-            break;
-        case '*':
-            break;
-        case '/':
-            break;
-        case '(':
-            break;
-        case ')':
-            break;
-        case '^':
-            break;
-        case '=':
-            break;
-}
-}
+
+
--- tokenizer.h
+++ tokenizer.h
@@ -9,32 +9,32 @@
 #define TRUE 1
 #define FALSE 0
 
-#define MULT_OP "*"
+#define MULT_OP '*'
 #define ADD_OP '+'
-#define SUB_OP "-"
-#define DIV_OP "/"
-#define ASSIGN_OP "="
-#define LEFT_PAREN "("
-#define RIGHT_PAREN ")"
-#define EXPON_OP "^"
-#define LESS_THEN_OP "<"
-#define LESS_THEN_OR_EQUAL_OP "<="
-#define GREATER_THEN_OR_EQUAL_OP ">="
-#define EQUALS_OP "=="
-#define NOT_OP "!"
-#define GREATER_THEN_OP ">"
-#define NOT_EQUAL_OP "!="
-#define SEMI_COLON ";"
+#define SUB_OP '-'
+#define DIV_OP '/'
+#define ASSIGN_OP '='
+#define LEFT_PAREN '('
+#define RIGHT_PAREN ')'
+#define EXPON_OP '^'
+#define LESS_THEN_OP '<'
+#define LESS_THEN_OR_EQUAL_OP '<='
+#define GREATER_THEN_OR_EQUAL_OP '>='
+#define EQUALS_OP '=='
+#define NOT_OP '!'
+#define GREATER_THEN_OP '>'
+#define NOT_EQUAL_OP '!='
+#define SEMI_COLON ';'
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define INT_LITERAL "[0-9]"
+#define INT_LITERAL 'INT_LITERAL'
 
 /**
 * add comment
 */
-char* remove_whitespace(char* );
+
 void get_token(char *); 
 void identify_lexeme(char*);
 void get_token_type(char);
